0d131bfefef52df888825a4fec2978e81dca53f2
YCC96 <yordy.cruz_96@outlook.es>
2021-03-11 16:12:11 -0600

    cm inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ContenidoComponent } from './components/contenido/contenido.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
+
+const routes: Routes = [
+  { path:'home', component: ContenidoComponent,
+    children: [
+      { path: 'peliculas', component: PeliculasComponent }
+    ]
+  },
+  { path:'', pathMatch:'full', redirectTo:'home' },
+  { path:'**', pathMatch:'full', redirectTo:'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ContenidoComponent } from './components/contenido/contenido.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ContenidoComponent,
+    PeliculasComponent
   ],
   imports: [
     BrowserModule,
